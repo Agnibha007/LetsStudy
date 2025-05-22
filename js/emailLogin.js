d69d684c6bfd05d8d77fc8f041fb70ad0791c303
Agnibha Mukherjee <b5x003agnibha.mukherjee@gmail.com>
2025-05-22 09:21:50 +0530

    commitmsg

---
+++ js/emailLogin.js
@@ -0,0 +1,39 @@
+function sendOTP() {
+    const email = document.getElementById("email").value;
+    if (!email.endsWith(".com")) {
+        alert("Please enter a valid email");
+    }
+    else {
+        alert("Please Wait");
+        document.getElementById("otp").disabled = false;
+        document.getElementById("btn-otp").disabled = false;
+        document.getElementsByClassName("verify-otp")[0].style.color = "white";
+        document.getElementById("otp").placeholder = "Enter OTP";
+        document.getElementById("name").placeholder = document.getElementById("name").value;
+        document.getElementById("email").placeholder = document.getElementById("email").value;
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("name").disabled = true;
+        document.getElementById("email").disabled = true;
+        document.getElementsByClassName("send-otp")[0].disabled = true;
+        document.getElementsByClassName("send-otp")[0].style.color = "grey";
+        alert("OTP sent successfully");
+    }
+}
+
+function verifyOTP() {
+    if (document.getElementsByClassName("verify-otp")[0].style.color === "white") {
+        const otp = document.getElementById("otp").value;
+        if (otp === "666333") {
+            alert("OTP verified successfully");
+        }
+        else {
+            alert("Incorrect OTP");
+        }
+        document.getElementById("otp").value = "";
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        window.location.href = "../html/classSelection.html";
+    }
+
+}
