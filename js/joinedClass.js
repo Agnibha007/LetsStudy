2ce1d690922dfb314c5dedcab1b2ae0f919785c6
Agnibha Mukherjee <b5x003agnibha.mukherjee@gmail.com>
2025-06-10 12:49:13 +0530

    sixteenth

--- js/joinedClass.js
+++ js/joinedClass.js
@@ -1,15 +1,34 @@
 let currentRoom = localStorage.getItem("selectedRoom");
 // console.log(currentRoom);
-window.onload = function() {
+window.onload = function () {
     if (currentRoom !== null) {
         document.getElementById(currentRoom).style.backgroundColor = "grey";
-        document.getElementsByClassName("main-room")[0].innerHTML = "Room " + currentRoom + "<br>(You're here)";   
+        document.getElementsByClassName("main-room")[0].innerHTML = "Room " + currentRoom + "<br>(You're here)";
     }
 }
 
+document.addEventListener('keydown', function (event) {
+    let key = event.key;
+    if (key >= '1' && key <= '9') {
+        joinRoom(parseInt(key));
+    } else if (key === '0') {
+        joinRoom(10);
+    } else if (key === '-') {
+        joinRoom(11);
+    } else if (key === '=') {
+        joinRoom(12);
+    } else if (key.toLowerCase() === 'q') {
+        joinRoom(13);
+    } else if (key.toLowerCase() === 'w') {
+        joinRoom(14);
+    } else if (key.toLowerCase() === 'e') {
+        joinRoom(15);
+    }
+});
+
 function joinRoom(roomNumber) {
     //Insert backend
-    document.getElementsByClassName("main-room")[0].innerHTML = "Room " + roomNumber + "<br>(You're here)";   
+    document.getElementsByClassName("main-room")[0].innerHTML = "Room " + roomNumber + "<br>(You're here)";
     if (currentRoom !== null) {
         document.getElementById(currentRoom).style.backgroundColor = "white";
     }
